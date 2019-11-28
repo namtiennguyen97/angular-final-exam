e1ef523034e4d6688d0df366f63b3f9fa874cee1
namnguyen <toilanam97@gmail.com>
2019-11-28 08:15:51 +0700

    nothing to change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BookComponent} from './book/book.component';
+import {BookAddComponent} from './book-add/book-add.component';
+import {BookEditComponent} from './book-edit/book-edit.component';
+import {BookDetailComponent} from './book-detail/book-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BookComponent},
+  {path: 'add', component: BookAddComponent},
+  {path: 'edit/:id', component: BookEditComponent},
+  {path: 'detail/:id', component: BookDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import {HttpClientModule} from '@angular/common/http';
+import { BookAddComponent } from './book-add/book-add.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookAddComponent,
+    BookEditComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
